252dc8c7f3301bb59f87afdae1778e6dc732eb67
Dedi Triyadi <dedi.triyadi@pinteraktif.com>
2024-12-13 18:00:13 +0700

    Change Jenkinsfile for kube version v.0.4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,7 @@ public class Main {
     // Handler untuk permintaan HTTP
     static class HelloHandler implements HttpHandler {
         public void handle(HttpExchange exchange) throws IOException {
-            String response = "Hello, dedi belajar Kubernetes : v.0.3!";
+            String response = "Hello, dedi belajar Kubernetes : v.0.4!";
             exchange.sendResponseHeaders(200, response.getBytes().length);
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
